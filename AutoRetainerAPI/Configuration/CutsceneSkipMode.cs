66907dd3fe785aa0bd2286c126738596e7f15c4a
Limiana <5073202+Limiana@users.noreply.github.com>
2026-04-23 02:46:36 +0300

    Update

---
+++ AutoRetainerAPI/Configuration/CutsceneSkipMode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoRetainerAPI.Configuration;
+
+public enum CutsceneSkipMode
+{
+    Never,
+    When_Multi_Mode_is_on,
+    Always,
+}
